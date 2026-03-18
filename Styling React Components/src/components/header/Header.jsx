d971ea20755376b00c32b892d5afc825cbb0eb60
restugedepurnama <restumelenoy83@gmail.com>
2026-03-18 10:10:00 +0700

    modified some files

---
+++ Styling React Components/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import logo from "../../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img
+        src={logo}
+        alt="A Canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold text-center tracking-widest uppercase text-amber-800">
+        ReactArt
+      </h1>
+      <p>A Community of Artist and art-lovers.</p>
+    </header>
+  );
+}
