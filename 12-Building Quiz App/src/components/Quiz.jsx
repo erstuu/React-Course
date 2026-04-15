1df6a24d76364914037c3b758bdc615896fd9e0f
restugedepurnama <restumelenoy83@gmail.com>
2026-04-15 10:08:04 +0700

    added new file to populate quiz body and populate it through quiz component

--- 12-Building Quiz App/src/components/Quiz.jsx
+++ 12-Building Quiz App/src/components/Quiz.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [usersAnswers, setUsersAnswers] = useState([]);
 
-  return <p>Currently ative question</p>;
+  const activeQuestionIndex = usersAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUsersAnswers();
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
